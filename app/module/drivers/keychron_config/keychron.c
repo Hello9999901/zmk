ad05dcc5b0964fb3fce2e34395a201f01c77740f
Hello9999901 <byran8855@gmail.com>
2024-03-05 22:05:46 -0500

    refactoring of the widget

--- app/module/drivers/keychron_config/keychron.c
+++ app/module/drivers/keychron_config/keychron.c
@@ -1,3 +1,12 @@
+/*
+ * Copyright (c) 2024 Byran Huang, Keychron
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+// Out-of-tree Keychron specific features
+// Structure based on: https://github.com/caksoylar/zmk-rgbled-widget by Cem Aksoylar
+
 #include <zephyr/device.h>
 #include <zephyr/devicetree.h>
 #include <zephyr/kernel.h>
@@ -23,23 +32,9 @@
 
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
-enum color_t {
-    LED_BLACK,   // 0b000
-    LED_RED,     // 0b001
-    LED_GREEN,   // 0b010
-    LED_YELLOW,  // 0b011
-    LED_BLUE,    // 0b100
-    LED_MAGENTA, // 0b101
-    LED_CYAN,    // 0b110
-    LED_WHITE    // 0b111
-};
-
-// a blink work item as specified by the color and duration
-struct blink_item {
-    enum color_t color;
-    uint16_t duration_ms;
-    bool first_item;
-    uint16_t sleep_ms;
+// a info work item as specified by state (extendable)
+struct info_item {
+    uint16_t state;
 };
 
 bool previous_underglow_on_off;
@@ -51,17 +46,17 @@ bool will_backlight_test = 0;
 int backlight_cycle = 0;
 bool testing_backlight = 0;
 
-// define message queue of blink work items, that will be processed by a separate thread
-K_MSGQ_DEFINE(led_msgq, sizeof(struct blink_item), 16, 4);
+// define message queue of info work items, that will be processed by a separate thread
+K_MSGQ_DEFINE(keychron_msgq, sizeof(struct info_item), 16, 4);
 
 #if IS_ENABLED(CONFIG_ZMK_BLE)
 #if !IS_ENABLED(CONFIG_ZMK_SPLIT) || IS_ENABLED(CONFIG_ZMK_SPLIT_ROLE_CENTRAL)
 static int led_profile_listener_cb(const zmk_event_t *eh) {
     uint8_t profile_index = zmk_ble_active_profile_index();
     if (zmk_ble_active_profile_is_connected()) {
-        LOG_INF("Profile %d connected, blinking blue", profile_index);
+        LOG_INF("Profile %d connected", profile_index);
         if (previous_underglow_on_off == 0) {
-            LOG_ERR("Restored previous RGB On Off: %d", previous_underglow_on_off);
+            LOG_INF("Restored previous RGB On Off: %d", previous_underglow_on_off);
             zmk_rgb_underglow_select_effect(previous_underglow_state);
             zmk_rgb_underglow_off();
         } else {
@@ -70,11 +65,11 @@ static int led_profile_listener_cb(const zmk_event_t *eh) {
             zmk_rgb_underglow_on();
         }
     } else if (zmk_ble_active_profile_is_open()) {
-        LOG_INF("Profile %d open, blinking yellow", profile_index);
+        LOG_INF("Profile %d open", profile_index);
         previous_underglow_state = zmk_rgb_underglow_calc_effect(0);
         zmk_rgb_underglow_get_state(&previous_underglow_on_off);
-        LOG_ERR("Previous RGB state: %d", previous_underglow_state);
-        LOG_ERR("Previous RGB On Off: %d", previous_underglow_on_off);
+        LOG_INF("Previous RGB state: %d", previous_underglow_state);
+        LOG_INF("Previous RGB On Off: %d", previous_underglow_on_off);
         zmk_rgb_underglow_off();
         zmk_rgb_underglow_set_profile_number(profile_index);
         zmk_rgb_underglow_select_effect(4);
@@ -92,8 +87,8 @@ ZMK_SUBSCRIPTION(led_profile_listener, zmk_ble_active_profile_changed);
 #if IS_ENABLED(CONFIG_ZMK_BATTERY_REPORTING)
 #if !IS_ENABLED(CONFIG_ZMK_SPLIT) || IS_ENABLED(CONFIG_ZMK_SPLIT_ROLE_CENTRAL)
 uint8_t central_battery_level;
-static int led_battery_listener_cb(const zmk_event_t *eh) {
-    // check if we are in critical battery levels at state change, blink if we are
+static int battery_listener_cb(const zmk_event_t *eh) {
+    // check if we are in critical battery levels at state change, info if we are
     central_battery_level = as_zmk_battery_state_changed(eh)->state_of_charge;
     LOG_INF("Central battery level callback %d percent", central_battery_level);
 
@@ -111,18 +106,11 @@ static int led_battery_listener_cb(const zmk_event_t *eh) {
 
 static int system_checks_cb(const zmk_event_t *eh) {
     struct zmk_position_state_changed *ev = as_zmk_position_state_changed(eh);
-    LOG_INF("Custom listener: ZMK position state at %d is %d!", ev->position, ev->state);
-
-    struct zmk_endpoint_instance endpoint_instance = zmk_endpoints_selected();
-    enum zmk_transport selected_transport = endpoint_instance.transport;
-    LOG_WRN("Current endpoint: %d", selected_transport);
-
-    LOG_WRN("Current central battery level: %d", central_battery_level);
 
     if (will_reset == 1) {
-        LOG_INF("ENDED RESET");
-        struct blink_item blink = {.duration_ms = 4321};
-        k_msgq_put(&led_msgq, &blink, K_NO_WAIT);
+        LOG_INF("Reset process terminated");
+        struct info_item info = {.state = 0};
+        k_msgq_put(&keychron_msgq, &info, K_NO_WAIT);
         will_reset = 0;
     }
 
@@ -178,8 +166,8 @@ static int system_checks_cb(const zmk_event_t *eh) {
     if (reset_latch[0] == 1 && reset_latch[1] == 1 && reset_latch[2] == 1) {
         will_reset = 1;
         LOG_INF("Factory reset %d %d %d", reset_latch[0], reset_latch[1], reset_latch[2]);
-        struct blink_item blink = {.duration_ms = 1111};
-        k_msgq_put(&led_msgq, &blink, K_NO_WAIT);
+        struct info_item info = {.state = 1};
+        k_msgq_put(&keychron_msgq, &info, K_NO_WAIT);
     }
 
     if (backlight_test_latch[0] == 1 && backlight_test_latch[1] == 1 &&
@@ -187,8 +175,8 @@ static int system_checks_cb(const zmk_event_t *eh) {
         will_reset = 1;
         LOG_INF("Backlight test %d %d %d", reset_latch[0], reset_latch[1], reset_latch[2]);
         if (testing_backlight == 0) {
-            struct blink_item blink = {.duration_ms = 2222};
-            k_msgq_put(&led_msgq, &blink, K_NO_WAIT);
+            struct info_item info = {.state = 2};
+            k_msgq_put(&keychron_msgq, &info, K_NO_WAIT);
         }
     }
 
@@ -201,17 +189,17 @@ static int system_checks_cb(const zmk_event_t *eh) {
     return 0;
 }
 
-// run led_battery_listener_cb on battery state change event
-ZMK_LISTENER(led_battery_listener, led_battery_listener_cb);
+// run battery_listener_cb on battery state change event
+ZMK_LISTENER(battery_listener, battery_listener_cb);
 ZMK_LISTENER(key_press_listener, system_checks_cb);
-ZMK_SUBSCRIPTION(led_battery_listener, zmk_battery_state_changed);
+ZMK_SUBSCRIPTION(battery_listener, zmk_battery_state_changed);
 ZMK_SUBSCRIPTION(key_press_listener, zmk_position_state_changed);
 
 #else
 
 uint8_t peripheral_battery_level;
-static int led_battery_peripheral_listener_cb(const zmk_event_t *eh) {
-    // check if we are in critical battery levels at state change, blink if we are
+static int battery_peripheral_listener_cb(const zmk_event_t *eh) {
+    // check if we are in critical battery levels at state change, info if we are
     peripheral_battery_level = as_zmk_battery_state_changed(eh)->state_of_charge;
     LOG_INF("Peripheral battery level callback %d percent", peripheral_battery_level);
 
@@ -227,8 +215,7 @@ static int led_battery_peripheral_listener_cb(const zmk_event_t *eh) {
     return 0;
 }
 static int system_checks_cb(const zmk_event_t *eh) {
-    struct zmk_position_state_changed *ev = as_zmk_position_state_changed(eh);
-    LOG_WRN("Current peripheral battery level: %d", peripheral_battery_level);
+    LOG_INF("Current peripheral battery level: %d", peripheral_battery_level);
     if (peripheral_battery_level <= 8) {
         LOG_ERR("Peripheral battery level %d, blinking red for critical", peripheral_battery_level);
         zmk_rgb_underglow_on();
@@ -239,32 +226,30 @@ static int system_checks_cb(const zmk_event_t *eh) {
 }
 
 ZMK_LISTENER(key_press_listener, system_checks_cb);
-ZMK_LISTENER(led_battery_peripheral_listener, led_battery_peripheral_listener_cb);
-ZMK_SUBSCRIPTION(led_battery_peripheral_listener, zmk_battery_state_changed);
+ZMK_LISTENER(battery_peripheral_listener, battery_peripheral_listener_cb);
+ZMK_SUBSCRIPTION(battery_peripheral_listener, zmk_battery_state_changed);
 ZMK_SUBSCRIPTION(key_press_listener, zmk_position_state_changed);
 
 #endif // IS_ENABLED(CONFIG_ZMK_BATTERY_REPORTING)
 
 #endif // !IS_ENABLED(CONFIG_ZMK_SPLIT) || IS_ENABLED(CONFIG_ZMK_SPLIT_ROLE_CENTRAL)
 
-extern void led_thread(void *d0, void *d1, void *d2) {
+extern void keychron_config_thread(void *d0, void *d1, void *d2) {
     ARG_UNUSED(d0);
     ARG_UNUSED(d1);
     ARG_UNUSED(d2);
 
     while (true) {
-        // wait until a blink item is received and process it
-        struct blink_item blink;
-        k_msgq_get(&led_msgq, &blink, K_FOREVER);
-        LOG_DBG("Got a blink item from msgq, color %d, duration %d", blink.color,
-                blink.duration_ms);
-        LOG_INF("LED THREAD SLEEPING FOR 3 SECONDS");
+        // wait until a info item is received and process it
+        struct info_item info;
+        k_msgq_get(&keychron_msgq, &info, K_FOREVER);
+        LOG_DBG("Got a info item from msgq, state %d", info.state);
+        LOG_INF("Config thread 3 second check");
         k_msleep(3000);
-        k_msgq_peek(&led_msgq, &blink);
-        LOG_DBG("PEEKED a link item from msgq, color %d, duration %d", blink.color,
-                blink.duration_ms);
-        if (blink.duration_ms == 1111) {
-            LOG_WRN("WARNING: RESETTING %d", blink.duration_ms);
+        k_msgq_peek(&keychron_msgq, &info);
+        LOG_DBG("Peekd a info item from msgq, state %d", info.state);
+        if (info.state == 1) {
+            LOG_WRN("Resetting, state: %d", info.state);
             // Preparing to reset animation
             zmk_rgb_underglow_on();
             zmk_rgb_underglow_select_effect(6);
@@ -279,8 +264,8 @@ extern void led_thread(void *d0, void *d1, void *d2) {
 #endif
             zmk_reset(ZMK_RESET_WARM);
         }
-        if (blink.duration_ms == 2222) {
-            LOG_WRN("WARNING: TESTING BACKLIGHT %d", blink.duration_ms);
+        if (info.state == 2) {
+            LOG_WRN("Underglow debugging, state: %d", info.state);
             zmk_rgb_underglow_select_effect(0); // solid color
             zmk_rgb_underglow_change_sat(100);
             zmk_rgb_underglow_change_brt(100);
@@ -288,10 +273,10 @@ extern void led_thread(void *d0, void *d1, void *d2) {
             zmk_rgb_underglow_on();
             testing_backlight = 1;
         }
-        k_msgq_purge(&led_msgq);
+        k_msgq_purge(&keychron_msgq);
     }
 }
 
 // define led_thread with stack size 512, start running it 500 ms after boot
-K_THREAD_DEFINE(led_tid, 512, led_thread, NULL, NULL, NULL, K_LOWEST_APPLICATION_THREAD_PRIO, 0,
-                500);
+K_THREAD_DEFINE(keychron_tid, 512, keychron_config_thread, NULL, NULL, NULL,
+                K_LOWEST_APPLICATION_THREAD_PRIO, 0, 500);
